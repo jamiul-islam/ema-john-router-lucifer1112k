fc1dd92de409f126cbf6ad4f0bf69e30fdd6fef2
lucifer1112k <jamiulislamjami@hotmail.com>
2021-10-01 21:18:12 +0600

    changed-one-line

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const ReviewItem = (props) => {
-    const { name, price, quantity, key } = props.product;
-    const { handleRemove } = props;
-    return (
-        <div className="product">
-            <div>
-                <h4 className="product-name">{name}</h4>
-                <p>Price: {price}</p>
-                <p>Quantity: {quantity}</p>
-                <button onClick={() => handleRemove(key)} className="btn-regular">Remove</button>
-            </div>
-        </div>
-    );
+  const { name, price, quantity, key } = props.product;
+  const { handleRemove } = props;
+  return (
+    <div className="product">
+      <div>
+        <small>this is a new product</small>
+        <h4 className="product-name">{name}</h4>
+        <p>Price: {price}</p>
+        <p>Quantity: {quantity}</p>
+        <button onClick={() => handleRemove(key)} className="btn-regular">
+          Remove
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ReviewItem;
